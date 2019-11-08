d4e5c4e532e7fc238509ff6faf29297170b4dace
gboh1877 <george.o.hughes94@gmail.com>
2019-11-08 15:34:20 +0000

    gah

--- .travis/deploy_docker_image.sh
+++ .travis/deploy_docker_image.sh
@@ -3,5 +3,5 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PWD
 TAG="latest"
 
-docker build -t lorrenzofreemano/scrapeme:$TAG
+docker build -t lorrenzofreemano/scrapeme:$TAG .
 docker push lorrenzofreemano/scrapeme:$TAG
